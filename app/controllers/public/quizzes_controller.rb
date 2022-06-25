50d9bfa24f8fdf1fe1392d9d1e7e831e9bb68882
hassy1220 <hashihassy.1220@gmail.com>
2022-06-25 16:46:53 +0000

    kaminariクイズ一覧

--- app/controllers/public/quizzes_controller.rb
+++ app/controllers/public/quizzes_controller.rb
@@ -1,9 +1,9 @@
 class Public::QuizzesController < ApplicationController
 
   def index
-    quizs = Quiz.includes(:questions).where(status: true)
-    # quizs = Quiz.where(status: true)
-    @quizs = quizs.select {|item| item.questions.count > 1 }
+    @quizs = Quiz.includes(:questions).where(status: true).page(params[:page]).per(8)
+    # # quizs = Quiz.where(status: true)
+    # @quizs = quizs.select {|item| item.questions.count > 1 }
   end
 
 end
