59299e002efdfedce41590c5bbf676703ac09fb0
hassy1220 <hashihassy.1220@gmail.com>
2022-07-03 14:09:39 +0000

    画面遷移時警告発生イベント

--- app/javascript/packs/answers/answer.js
+++ app/javascript/packs/answers/answer.js
@@ -48,14 +48,45 @@
 
   var onBeforeunloadHandler = function(e) {
     e.returnValue = 'ちょっと待ってくださいよ。まだダメですよ。';
-};
-// イベントを登録
-window.addEventListener('beforeunload', onBeforeunloadHandler, false);
+  };
+  // イベントを登録
+  window.addEventListener('beforeunload', onBeforeunloadHandler, false);
 
-// 変数formにform_withの内容を取得(htmlcollectionにて)
-var form = document.getElementById('form_id');
-// formの中のsubmitイベントを追加
-form.addEventListener('submit', function(e) {
-    // イベントを削除(リロード警告)
+  // 変数formにform_withの内容を取得(htmlcollectionにて)
+  var form = document.getElementById('form_id');
+
+  // formの中のsubmitイベントを追加
+  form.addEventListener('submit', function(e) {
+    // イベントを削除
     window.removeEventListener('beforeunload', onBeforeunloadHandler, false);
-}, false);
+  }, false);
+
+
+  // これ以降はクイズ回答中にヘッダーで遷移を試みた場合に警告文イベントを削除
+  var reload_1 = document.getElementById('reload_of_1');
+  // formの中のsubmitイベントを追加
+  reload_1.addEventListener('click', function(e) {
+        // イベントを削除(リロード警告)
+    window.removeEventListener('beforeunload', onBeforeunloadHandler, false);
+  }, false);
+
+  var reload_2 = document.getElementById('reload_of_2');
+  // formの中のsubmitイベントを追加
+  reload_2.addEventListener('click', function(e) {
+        // イベントを削除(リロード警告)
+    window.removeEventListener('beforeunload', onBeforeunloadHandler, false);
+  }, false);
+
+  var reload_3 = document.getElementById('reload_of_3');
+  // formの中のsubmitイベントを追加
+  reload_3.addEventListener('click', function(e) {
+        // イベントを削除(リロード警告)
+    window.removeEventListener('beforeunload', onBeforeunloadHandler, false);
+  }, false);
+
+  var reload_4 = document.getElementById('reload_of_4');
+  // formの中のsubmitイベントを追加
+  reload_4.addEventListener('click', function(e) {
+        // イベントを削除(リロード警告)
+    window.removeEventListener('beforeunload', onBeforeunloadHandler, false);
+  }, false);
